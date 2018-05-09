815af71a3ce6808da957c9f97ef0e4142a467af7
Jay Shah <reversearrow@gmail.com>
2018-05-09 06:43:23 -0400

    Feat
    - Consolidated Two different view functions into one.

--- app/views.py
+++ app/views.py
@@ -1,36 +1,35 @@
 from flask import Blueprint, render_template, url_for, redirect, request
 from models import db, Categories, Items
+import re
+
 
 catalog_bp = Blueprint('catalog', __name__)
 
 
-@catalog_bp.route('/')
-def root():
-    categories = Categories.query.order_by('name').all()
-    items = Items.query.order_by('created_at').all()
-    return render_template('root.html', categories=categories, items=items)
-
-
 @catalog_bp.route('/catalog/<string:category>/items')
-def display_category_items(category):
+@catalog_bp.route('/')
+def root(category='default'):
     categories = Categories.query.order_by('name').all()
-    category = Categories.query.filter_by(name=category).first()
-    print category.items
-    return "category"
+    if category == 'default':
+        items = Items.query.order_by('created_at').all()
+        return render_template('root.html', categories=categories, category='Latest', items=items)
+    else:
+        category = Categories.query.filter_by(name=category).first_or_404()
+        return render_template('root.html', categories=categories, category=category.name, items=category.items)
 
 
 @catalog_bp.route('/catalog/add', methods=['GET', 'POST'])
 def add():
     categories = Categories.query.order_by('name').all()
     if request.method == 'POST':
-        name = request.form.get('title').replace(" ", "")
-        description = request.form.get('description').replace(" ", "")
+        name = request.form.get('title')
+        description = request.form.get('description')
         category = request.form.get('categories')
-        print category
-        if not name or name == "":
-            return render_template('items.html', title=name, categories=categories, error="Title is not valid!")
-        if not description or description == "":
-            return render_template('items.html', description=description, categories=categories, error="Description is not valid!")
+        match_empty_regex = r' +'
+        if re.match(match_empty_regex, name) or name == "":
+            return render_template('items.html', title=name, description=description, categories=categories, error="Title is not valid!")
+        if re.match(match_empty_regex, description) or description == "":
+            return render_template('items.html', title=name, description=description, categories=categories, error="Description is not valid!")
         else:
             category = Categories.query.filter_by(name=category).first()
             new_item = Items(name=name, description=description)
